03231775247325b72e0aa3c3687dab46d52b6e70
itzblacckk <editorblacckk@gmail.com>
2024-09-24 13:14:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,56 @@
- 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('canvas3d') });
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-renderer.setClearColor(0xffffff, 1);
-const geometry = new THREE.TorusKnotGeometry(100, 30, 50, 16);
-const material = new THREE.MeshNormalMaterial({ wireframe: true });
-const torusKnot = new THREE.Mesh(geometry, material);
-scene.add(torusKnot);
-
-camera.position.z = 50;
-
-function animate() {
-    requestAnimationFrame(animate);
-
-
-    torusKnot.rotation.x += 0.01;
-    torusKnot.rotation.y += 0.01;
-
-    renderer.render(scene, camera);
-}
-animate();
-
-
-window.addEventListener('resize', () => {
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
 });
+
+
+function page4Animation() {
+    var elemC = document.querySelector("#elem-container")
+    var fixed = document.querySelector("#fixed-image")
+    elemC.addEventListener("mouseenter", function() {
+        fixed.style.display = "block"
+    })
+    elemC.addEventListener("mouseleave", function() {
+        fixed.style.display = "none"
+    })
+
+    var elems = document.querySelectorAll(".elem")
+    elems.forEach(function(e) {
+        e.addEventListener("mouseenter", function() {
+            var image = e.getAttribute("data-image")
+            fixed.style.backgroundImage = url(${ image })
+        })
+    })
+}
+
+function swiperAnimation() {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 100,
+    });
+}
+function menuAnimation() {
+
+    var menu = document.querySelector("nav h3")
+    var full = document.querySelector("#full-scr")
+    var navimg = document.querySelector("nav img")
+    var flag = 0
+    menu.addEventListener("click", function() {
+        if (flag == 0) {
+            full.style.top = 0
+            navimg.style.opacity = 0
+            flag = 1
+        } else {
+            full.style.top = "-100%"
+            navimg.style.opacity = 1
+            flag = 0
+        }
+    })
+}
+
+
+
+swiperAnimation()
+page4Animation()
+menuAnimation()
